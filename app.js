e74609f2d18665bd0924137244111617211380d4
Miao <miao@163.com>
2017-07-21 20:38:51 +0800

    app.js commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//app.js
+App({
+  // 当页面启动时触发该事件
+  onLaunch: function() {
+    console.log('running demo')
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+
+// 读取当前用户信息
+  getUserInfo: function(cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function(res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+        }
+      })
+    }
+  },
+
+  globalData: {
+    userInfo: null
+  }
+})
